3fa2f070fcaba6e4a31a6b7a9265caced69535b3
Dylan Lester <s3542871@student.rmit.edu.au>
2017-08-20 03:10:55 +1000

    Fix image bug and mobile menu bug (js asset ref)

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -8,7 +8,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <!-- Javascript -->
-    <script type="text/javascript" src="js/app.js"></script>
+    <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
     <!-- CSS -->
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
